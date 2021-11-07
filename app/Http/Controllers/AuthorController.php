ce2369cdd77ed9a9c288a1687f42050284976fcb
KOKI <coatchtech0613@gmail.com>
2021-11-07 11:19:45 +0900

    修正

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -8,23 +9,48 @@ class AuthorController extends Controller
 {
     public function index()
     {
-        $items = DB::table('authors')->get();
+        $items = DB::select('select * from authors');
         return view('index', ['items' => $items]);
     }
-
     public function add()
     {
         return view('add');
     }
-
     public function create(Request $request)
     {
         $param = [
             'name' => $request->name,
             'age' => $request->age,
-            'nationality' => $request->nationality,
         ];
-        DB::table('authors')->insert($param);
+        DB::insert('insert into authors (name, age) values (:name, :age)', $param);
+        return redirect('/');
+    }
+    public function edit(Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from authors where id = :id', $param);
+        return view('edit', ['form' => $item[0]]);
+    }
+    public function update(Request $request)
+    {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'age' => $request->age,
+        ];
+        DB::update('update authors set name =:name, age =:age where id =:id', $param);
+        return redirect('/');
+    }
+    public function delete(Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from authors where id = :id', $param);
+        return view('delete', ['form' => $item[0]]);
+    }
+    public function remove(Request $request)
+    {
+        $param = ['id' => $request->id];
+        DB::delete('delete from authors where id =:id', $param);
         return redirect('/');
     }
 }
